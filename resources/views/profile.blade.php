fc2a17c4a1c75e61e1166182c86e770d5c58bafe
KhalidChawtany <khalidchawtany@yahoo.com>
2022-04-06 11:31:23 +0300

    inital commit

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width" />
+    <title>Welcome</title>
+  </head>
+  <body>
+
+    <img src="/myphoto.png" alt="photo"/>
+
+    <div class="bio">
+      My biography
+      
+    </div>
+  </body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width" />
+    <title>Welcome</title>
+  </head>
+  <body>
+    Click the followig to see my beautifull profile :)
+    <a href="/profile">Profile</a>
+  </body>
+</html>
